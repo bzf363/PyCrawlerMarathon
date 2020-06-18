f3a37eb111ea1df6881f37f60e66b69ec20a854d
bzf363 <bzf363@outlook.com>
2020-06-18 13:23:43 +0800

    20200618_ex2_v2

--- homework/20200618_ex2.py
+++ homework/20200618_ex2.py
@@ -30,13 +30,40 @@ with open ('/Users/chenshihti/Desktop/example.csv','r') as osfile3:
     listreport =list(csvReader) #轉list才可以看資料
     #for i in range (1,len(listreport),1):
     # 1. 取出班次一的每一個時間
-    print(listreport[1][5:15:1])
+    for i in range (0,len(listreport),1):
+        print(listreport[i][5])
+    
     # 2. 將班次一的每一個時間用一個變數保存
-    datastorage1=listreport[1][5:15:1]
+    
     datastorage2=[]
+    for i in range (0,len(listreport),1):
+        datastorage2.append(listreport[i][5])
     # 3. 將所有班次和其每一個時間用一個變數保存
-    for i in range (1,len(listreport),1):
-        datastorage2.append(listreport[i][5:15:1])
+        
+    data2=[]
+    data3=[]
+    data4=[]
+    data5=[]
+    data6=[]
+    data7=[]
+    data8=[]
+    data9=[]
+    data10=[]
+    data11=[]
+    for i in range (0,len(listreport),1):
+        data2.append(listreport[i][6])
+        data3.append(listreport[i][7])
+        data4.append(listreport[i][8])
+        data5.append(listreport[i][9])
+        data6.append(listreport[i][10])
+        data7.append(listreport[i][11])
+        data8.append(listreport[i][12])
+        data9.append(listreport[i][13])
+        data10.append(listreport[i][14])
+        data11.append(listreport[i][15])
+
+
+
         
     
         
